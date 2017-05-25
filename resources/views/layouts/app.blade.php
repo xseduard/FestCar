2e2a3508f5f323cc9401cf2d4892cf52e36d98d5
Eduard Ramos <xseduard@gmail.com>
2017-05-25 13:31:52 -0500

    layouts adminLTE

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,24 @@
 
     @yield('css')
 </head>
+<style type="text/css">
+    
+.skin-blue .main-header .navbar {
+    background: #0095d8 !important;
+    background: -moz-linear-gradient(left,  #0095d8 0%, #00b0a3 100%) !important;
+    background: -webkit-linear-gradient(left,  #0095d8 0%,#00b0a3 100%) !important;
+    background: linear-gradient(to right,  #0095d8 0%,#00b0a3 100%) !important;
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#0095d8', endColorstr='#00b0a3',GradientType=1 ) !important;
+
+}
+.skin-blue .main-header .logo {
+    background: #0095D8;
+}
+.skin-blue .main-header li.user-header {
+    background: #00B0A3;
+}
+  
+</style>
 
 <body class="skin-blue sidebar-mini">
 @if (!Auth::guest())
@@ -25,7 +43,7 @@
 
             <!-- Logo -->
             <a href="#" class="logo">
-                <b>InfyOm</b>
+                <b>T</b>ranseba
             </a>
 
             <!-- Header Navbar -->
@@ -42,28 +60,28 @@
                             <!-- Menu Toggle Button -->
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                                 <!-- The user image in the navbar-->
-                                <img src="http://infyom.com/images/logo/blue_logo_150x150.jpg"
-                                     class="user-image" alt="User Image"/>
+                                <img  src="{{ asset('/multimedia/profiles/Estefania.jpg') }}"
+                                     class="user-image" alt="imagen de usuario"/>
                                 <!-- hidden-xs hides the username on small devices so only the image appears. -->
-                                <span class="hidden-xs">{!! Auth::user()->name !!}</span>
+                                <span class="hidden-xs">{!! Auth::user()->nombres !!}</span>
                             </a>
                             <ul class="dropdown-menu">
                                 <!-- The user image in the menu -->
                                 <li class="user-header">
-                                    <img src="http://infyom.com/images/logo/blue_logo_150x150.jpg"
-                                         class="img-circle" alt="User Image"/>
+                                    <img  src="{{ asset('/multimedia/profiles/Estefania.jpg') }}"
+                                         class="img-circle" alt="Imagen de usuario"/>
                                     <p>
-                                        {!! Auth::user()->name !!}
-                                        <small>Member since {!! Auth::user()->created_at->format('M. Y') !!}</small>
+                                        {!! Auth::user()->nombres," ", Auth::user()->apellidos  !!}
+                                        <small>Miembro desde {!! Auth::user()->created_at->format('M. Y') !!}</small>
                                     </p>
                                 </li>
                                 <!-- Menu Footer-->
                                 <li class="user-footer">
                                     <div class="pull-left">
-                                        <a href="#" class="btn btn-default btn-flat">Profile</a>
+                                        <a href="#" class="btn btn-default btn-flat">Perfil</a>
                                     </div>
                                     <div class="pull-right">
-                                        <a href="{!! url('/logout') !!}" class="btn btn-default btn-flat">Sign out</a>
+                                        <a href="{!! url('/logout') !!}" class="btn btn-default btn-flat">Cerrar session</a>
                                     </div>
                                 </li>
                             </ul>
@@ -87,6 +105,7 @@
 
     </div>
 @else
+<!-- USUARIO O INVITADO -->
     <nav class="navbar navbar-default navbar-static-top">
         <div class="container">
             <div class="navbar-header">
