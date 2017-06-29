a6b4840ebb89da53c5c553c6056289bb7c815661
Eduard Ramos <xseduard@gmail.com>
2017-06-29 05:07:41 -0500

    pagos part 4

--- resources/views/pagos/fields.blade.php
+++ resources/views/pagos/fields.blade.php
@@ -1,13 +1,13 @@
 <!-- Cps Id Selector -->
 <div class="form-group col-sm-4 col-xl-4">
     {!! Form::label('cps_id', 'Contrato prestación de servicio') !!}
-    {!! Form::select('cps_id', $selectores['ContratoPrestacionServicio_id'], null, ['class' => 'form-control select2', 'style' => 'width: 100%', 'placeholder'=>'Seleccione...*']) !!}
+    {!! Form::select('cps_id', $selectores['ContratoPrestacionServicio_id'], null, ['class' => 'form-control select2', 'style' => 'width: 100%', 'placeholder'=>'Seleccione...*', 'required']) !!}
 </div>
 
 <!-- Contrato Vinculacion Selector -->
 <div class="form-group col-sm-8 col-xl-8">
     {!! Form::label('contrato_vinculacion_id', 'Contrato Vinculación / Vehículo') !!}
-    {!! Form::select('contrato_vinculacion_id', $selectores['ContratoVinculacion_id'], null, ['class' => 'form-control select2', 'style' => 'width: 100%', 'placeholder'=>'Seleccione...*']) !!}
+    {!! Form::select('contrato_vinculacion_id', $selectores['ContratoVinculacion_id'], null, ['class' => 'form-control select2', 'style' => 'width: 100%', 'placeholder'=>'Seleccione...*', 'required']) !!}
 </div>
 
 <!-- Fecha Planilla campo de fecha -->
@@ -15,7 +15,7 @@
     {!! Form::label('fecha_planilla', 'Fecha Planilla') !!}
     <div class="input-group">
         <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
-    	{!! Form::text('fecha_planilla', null, ['class' => 'form-control datepicker', 'placeholder' => 'AAAA-MM-DD']) !!}
+    	{!! Form::text('fecha_planilla', null, ['class' => 'form-control datepicker', 'placeholder' => 'AAAA-MM-DD', 'required']) !!}
     </div>
 </div>
 
@@ -24,7 +24,7 @@
     {!! Form::label('fecha_inicio', 'Semana Inicio') !!}
     <div class="input-group">
         <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
-    	{!! Form::text('fecha_inicio', null, ['class' => 'form-control datepicker', 'placeholder' => 'AAAA-MM-DD']) !!}
+    	{!! Form::text('fecha_inicio', null, ['class' => 'form-control datepicker', 'placeholder' => 'AAAA-MM-DD', 'required']) !!}
     </div>
 </div>
 
@@ -34,7 +34,7 @@
     {!! Form::label('fecha_final', 'Semana Final') !!}
     <div class="input-group">
         <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
-    	{!! Form::text('fecha_final', null, ['class' => 'form-control datepicker', 'placeholder' => 'AAAA-MM-DD']) !!}
+    	{!! Form::text('fecha_final', null, ['class' => 'form-control datepicker', 'placeholder' => 'AAAA-MM-DD', 'required']) !!}
     </div>
 </div>
 
@@ -64,13 +64,35 @@
 </div>
 
 <!-- /////////////////////////////////////////////// -->
+
+<div id="space-ruta"></div>
+<div class="col-sm-3 col-xl-3" style="padding-top: 25px">
+    <button id="agregar-ruta-predefinida" name="agregar-ruta-predefinida" type="button" class="btn btn-default btn-flat btn-primary-inverted btn-block"><i class="fa fa-plus"></i> Agregar Ruta Predefinida</button>
+</div>
+
+<div class="col-sm-3 col-xl-3" style="padding-top: 25px">
+    <button id="agregar-ruta-nueva" name="agregar-ruta-nueva" type="button" class="btn btn-default btn-flat btn-primary-inverted btn-block"><i class="fa fa-plus"></i> Agregar Ruta Nueva</button>
+</div>
+
+<!-- Subtotal Campo de texto -->
+<div class="form-group col-sm-offset-4 col-sm-2 col-xl-2">
+    {!! Form::label('subtotal', 'SubTotal (Rutas/Facturas)') !!}
+    <div class="input-group">
+        <span class="input-group-addon"><i class="fa fa-usd"></i></span>
+        {!! Form::text('subtotal', null, ['readonly', 'class' => 'form-control texto-right']) !!}
+    </div>
+</div>
+
+<div class="clearfix"></div>
+
+<!--
 <div class="rutabox">
 
-    {!! Form::text('predefinido[]', 'yes') !!}
+    {!! Form::hidden('predefinido[]', 'yes') !!}
 
 <div class="form-group col-sm-4">
     {!! Form::label('ruta_nombre[]', 'Nombre Ruta') !!}
-    {!! Form::select('ruta_nombre[]', $selectores['ruta_id'], null, ['id' => 'rutaname', 'class' => 'form-control select2 rutaname', 'style' => 'width: 100%', 'placeholder'=>'Seleccione...*']) !!}
+    {!! Form::select('ruta_nombre[]', $selectores['ruta_id'], null, ['id' => 'rutaname', 'class' => 'form-control select2 rutaname', 'style' => 'width: 100%', 'placeholder'=>'Seleccione...*', 'required']) !!}
 </div>
 
 <div class="form-group col-sm-2">
@@ -92,7 +114,7 @@
 <div class="form-group col-sm-2">
     {!! Form::label('cantidad[]', 'Cantidad') !!}
     <div class="input-group">
-        {!! Form::text('cantidad[]', null, ['class' => 'form-control texto-right', 'placeholder'=>'0']) !!}
+        {!! Form::text('cantidad[]', null, ['class' => 'form-control texto-right', 'placeholder'=>'0' , 'required']) !!}
         <span class="input-group-addon">Viajes</span>
     </div>
 </div>
@@ -101,76 +123,32 @@
     {!! Form::label('valor_final[]', 'Valor C/U') !!}
     <div class="input-group">
         <span class="input-group-addon"><i class="fa fa-usd"></i></span>
-        {!! Form::text('valor_final[]', 111111, ['class' => 'form-control texto-right', 'placeholder'=>'0']) !!}
+        {!! Form::text('valor_final[]', null, ['class' => 'form-control texto-right', 'placeholder'=>'0' , 'required']) !!}
+        <span class="input-group-btn eliminar-ruta"><button class="btn btn-danger" type="button"><i class="fa fa-times"></i></button></span>
     </div>
 </div>
 
 
 </div>
+<div class="clearfix"></div>
+-->
 <!-- ////////////////////    1   ////////////////////// -->
-<div class="clearfix"></div>
 
-<!-- /////////////////////   2  ////////////////////////// -->
-
-<div class="rutabox">
-
-    {!! Form::text('predefinido[]', 'yes') !!}
-
-    <div class="form-group col-sm-4">
-    {!! Form::label('ruta_nombre[]', 'Nombre Ruta') !!}
-    {!! Form::select('ruta_nombre[]', $selectores['ruta_id'], null, ['id' => 'rutaname', 'class' => 'form-control select2 rutaname', 'style' => 'width: 100%', 'placeholder'=>'Seleccione...*']) !!}
-</div>
-
-<div class="form-group col-sm-2">
-    {!! Form::label('distancia[]', 'Distancia') !!}
-    <div class="input-group">        
-        {!! Form::text('distancia[]', null, ['class' => 'form-control texto-right', 'placeholder'=>'0', 'readonly']) !!}
-        <span class="input-group-addon">Km</span>
-    </div>
-</div>
-
-<div class="form-group col-sm-2">
-    {!! Form::label('duracion[]', 'Duración (Ida-Vuelta)') !!}
-    <div class="input-group">
-        {!! Form::text('duracion[]', null, ['class' => 'form-control texto-right', 'placeholder'=>'0', 'readonly']) !!}
-        <span class="input-group-addon">minutos</span>
-    </div>
-</div>
-
-<div class="form-group col-sm-2">
-    {!! Form::label('cantidad[]', 'Cantidad') !!}
-    <div class="input-group">
-        {!! Form::text('cantidad[]', null, ['class' => 'form-control texto-right', 'placeholder'=>'0']) !!}
-        <span class="input-group-addon">Viajes</span>
-    </div>
-</div>
-
-<div class="form-group col-sm-2">
-    {!! Form::label('valor_final[]', 'Valor C/U') !!}
-    <div class="input-group">
-        <span class="input-group-addon"><i class="fa fa-usd"></i></span>
-        {!! Form::text('valor_final[]', 22222, ['class' => 'form-control texto-right', 'placeholder'=>'0']) !!}
-    </div>
-</div>
-
-</div>
-<!-- /////////////////////   2  ///////////////////// -->
-<div class="clearfix"></div>
 <!-- //////////////////////  3 ///////////////////////// -->
-
+<!--
 <div class="rutabox">
 
-    {!! Form::text('predefinido[]', 'not') !!}
+    {!! Form::hidden('predefinido[]', 'not') !!}
 
     <div class="form-group col-sm-4">
     {!! Form::label('ruta_nombre[]', 'Nombre Ruta') !!}
-    {!! Form::text('ruta_nombre[]', null, ['id' => 'new_rutaname', 'class' => 'form-control new_rutaname', 'placeholder'=>'Nueva Ruta']) !!}
+    {!! Form::text('ruta_nombre[]', null, ['id' => 'new_rutaname', 'class' => 'form-control new_rutaname', 'placeholder'=>'Nueva Ruta', 'required']) !!}
 </div>
 
 <div class="form-group col-sm-2">
     {!! Form::label('distancia[]', 'Distancia') !!}
     <div class="input-group">        
-        {!! Form::text('distancia[]', null, ['class' => 'form-control texto-right', 'placeholder'=>'0' ]) !!}
+        {!! Form::text('distancia[]', null, ['class' => 'form-control texto-right', 'placeholder'=>'0', 'required' ]) !!}
         <span class="input-group-addon">Km</span>
     </div>
 </div>
@@ -178,7 +156,7 @@
 <div class="form-group col-sm-2">
     {!! Form::label('duracion[]', 'Duración (Ida-Vuelta)') !!}
     <div class="input-group">
-        {!! Form::text('duracion[]', null, ['class' => 'form-control texto-right', 'placeholder'=>'0']) !!}
+        {!! Form::text('duracion[]', null, ['class' => 'form-control texto-right', 'placeholder'=>'0', 'required']) !!}
         <span class="input-group-addon">minutos</span>
     </div>
 </div>
@@ -186,7 +164,7 @@
 <div class="form-group col-sm-2">
     {!! Form::label('cantidad[]', 'Cantidad') !!}
     <div class="input-group">
-        {!! Form::text('cantidad[]', null, ['class' => 'form-control texto-right', 'placeholder'=>'0']) !!}
+        {!! Form::text('cantidad[]', 1, ['class' => 'form-control texto-right', 'placeholder'=>'0', 'required']) !!}
         <span class="input-group-addon">Viajes</span>
     </div>
 </div>
@@ -195,11 +173,14 @@
     {!! Form::label('valor_final[]', 'Valor C/U') !!}
     <div class="input-group">
         <span class="input-group-addon"><i class="fa fa-usd"></i></span>
-        {!! Form::text('valor_final[]', 22222, ['class' => 'form-control texto-right', 'placeholder'=>'0']) !!}
+        {!! Form::text('valor_final[]', null, ['class' => 'form-control texto-right', 'placeholder'=>'0', 'required']) !!}
     </div>
 </div>
 
 </div>
+<div class="clearfix"></div>
+
+-->
 <!-- ////////////////////////////////////////// -->
 
 
@@ -214,11 +195,12 @@
     {!! Form::label('text_cuatro_por_mil', '4x1000') !!}
     <div class="input-group">
       <span class="input-group-addon addon-custom" title="Habilitar / Desabilitar">
+      {!! Form::checkbox('cuatro_por_mil', '1', true) !!}
         {!! Form::hidden('cuatro_por_mil', false) !!}
-        {!! Form::checkbox('cuatro_por_mil', '1', true) !!}
+        
       </span>
       <span class="input-group-addon"><i class="fa fa-usd"></i></span>
-      {!! Form::text('text_cuatro_por_mil', null, ['class' => 'form-control texto-right', 'placeholder'=>'', 'readonly']) !!}
+      {!! Form::text('text_cuatro_por_mil', 0, ['class' => 'form-control texto-right', 'placeholder'=>'', 'readonly']) !!}
     </div>
 </div>
 
@@ -290,29 +272,33 @@
 
 
 
-<!-- Subtotal Campo de texto -->
-<div class="form-group col-sm-4 col-xl-4">
-    {!! Form::label('subtotal', 'SubTotal') !!}
-    {!! Form::text('subtotal', null, ['readonly', 'class' => 'form-control texto-right']) !!}
-</div>
-
-<div class="form-group col-sm-4 col-xl-4">
-    {!! Form::label('total_descuentos', 'Total Descuentos') !!}
-    {!! Form::text('total_descuentos', null, ['readonly', 'class' => 'form-control texto-right']) !!}
-</div>
 
 
-<!-- Total Campo de texto -->
-<div class="form-group col-sm-4 col-xl-4">
-    {!! Form::label('total', 'Total') !!}
-    {!! Form::text('total', null, ['readonly', 'class' => 'form-control texto-right', 'placeholder'=>'']) !!}
-</div>
+
 
 <!-- Submit Field -->
-<div class="form-group col-sm-12 col-xl-12">
+<div class="form-group col-sm-4 col-xl-4">
     {!! Form::button('<i class="glyphicon glyphicon-floppy-disk"></i> Guardar', [
         'type' => 'submit',
         'class' => 'btn btn-primary btn-flat'        
     ]) !!}
     <a href="{!! route('pagos.index') !!}" class="btn btn-default btn-flat">Cancelar</a>
+</div>
+
+<div class="form-group col-sm-4 col-xl-4">
+    <div class="input-group">
+        <span class="input-group-addon">Total Descuentos <i class="fa fa-usd"></i></span>
+        {!! Form::text('total_descuentos', null, ['readonly', 'class' => 'form-control texto-right']) !!}
+    </div>
+</div>
+
+
+<!-- Total Campo de texto -->
+
+<div class="form-group col-sm-4 col-xl-4">
+    
+    <div class="input-group">
+        <span class="input-group-addon">Total <i class="fa fa-usd"></i></span>
+        {!! Form::text('total', null, ['readonly', 'class' => 'form-control texto-right', 'placeholder'=>'']) !!}
+    </div>
 </div>
--- resources/views/pagos/script-create.blade.php
+++ resources/views/pagos/script-create.blade.php
@@ -1,6 +1,7 @@
 <script>
   
     $(document).ready(function () {  
+      sidebar_colapsado();
         $(".select2").select2({
           tags: false, // permite insertar texto
           language: {noResults: function() {return "No se encontraron coincidencias";}, searching: function() {return "Buscando..";}
@@ -20,21 +21,26 @@
         increaseArea: '20%' // optional
     });
 
+
+    // Iniciador rutas datos
      $(".rutaname").on('change',function(e){ 
           var sel_name = $(e.target).val();
-          var ruta     = buscar_ruta(sel_name);
-         
-
-                      
+          buscar_ruta(sel_name);          
         });
+
+     // Iniciador calculos totales
+     $(".texto-right").on('keyup',function(e){
+        calculos_totales();
+      });
+
 
 
     $(function(){
         // Crea el elemento factura
         $("#agregar-factura").on('click', function(){
-            var elemento = $('<div class="form-group col-sm-3" id="form-factura">{!! Form::label('factura[]', 'Factura') !!}<div class="input-group"><span class="input-group-addon">CR0000</span>{!! Form::text('factura[]', null, ['class' => 'form-control texto-right', 'required']) !!}<span class="input-group-btn eliminar-factura"><button class="btn btn-danger" type="button"><i class="fa fa-times"></i></button></span></div></div>');
+            var elemento_fac = $('<div class="form-group col-sm-3" id="form-factura">{!! Form::label('factura[]', 'Factura') !!}<div class="input-group"><span class="input-group-addon">CR0000</span>{!! Form::text('factura[]', null, ['class' => 'form-control texto-right', 'required']) !!}<span class="input-group-btn eliminar-factura"><button class="btn btn-danger" type="button"><i class="fa fa-times"></i></button></span></div></div>');
            // $("#form-factura").clone().appendTo("#space-factura").fadeIn("slow");
-            elemento.appendTo('#space-factura');
+            elemento_fac.appendTo('#space-factura');
             //console.log(elemento);
         });
      
@@ -47,9 +53,9 @@
 
         // Crea el elemento descuento
          $("#agregar-descuento").on('click', function(){
-            var elemento = $('<div class="form-descuento"><div class="form-group col-sm-6 col-xl-6">{!! Form::label('desc_tipo', 'Tipo / Valor (Descuento)') !!}<div class="input-group"><span class="input-group-addon select-addon-custom" style="width: 30% !important">{!! Form::select('desc_tipo[]', $selectores['descuento_id'], null, ['class' => 'form-control select2', 'style' => 'width: 100%', 'placeholder'=>'Seleccione...*', 'required']) !!}</span><span class="input-group-addon"><i class="fa fa-usd"></i></span>{!! Form::text('desc_valor[]', null, ['class' => 'form-control texto-right', 'required']) !!}<span class="input-group-btn eliminar-factura"><button class="btn btn-danger" type="button"><i class="fa fa-times"></i></button></span></div></div></div>');
+            var elemento_desc = $('<div class="form-descuento"><div class="form-group col-sm-6 col-xl-6">{!! Form::label('desc_tipo', 'Tipo / Valor (Descuento)') !!}<div class="input-group"><span class="input-group-addon select-addon-custom" style="width: 30% !important">{!! Form::select('desc_tipo[]', $selectores['descuento_id'], null, ['class' => 'form-control select2', 'style' => 'width: 100%', 'placeholder'=>'Seleccione...*', 'required']) !!}</span><span class="input-group-addon"><i class="fa fa-usd"></i></span>{!! Form::text('desc_valor[]', null, ['class' => 'form-control texto-right', 'required']) !!}<span class="input-group-btn eliminar-factura"><button class="btn btn-danger" type="button"><i class="fa fa-times"></i></button></span></div></div></div>');
            // $("#form-factura").clone().appendTo("#space-factura").fadeIn("slow");
-            elemento.appendTo('#space-descuento');
+            elemento_desc.appendTo('#space-descuento');
             //console.log(elemento);
             //reactivar select2
             $(".select2").select2({
@@ -60,7 +66,91 @@
               allowClear: true
             });
 
+            // Iniciador rutas datos otra vez DUPLICADO
+               $(".rutaname").on('change',function(e){ 
+                    var sel_name = $(e.target).val();
+                    buscar_ruta(sel_name);          
+                  });
+
+               // Iniciador calculos totales otra vez DUPLICADO
+               $(".texto-right").on('keyup',function(e){
+                  calculos_totales();
+                });
+
         });
+
+         // Crea el elemento factura
+        $("#agregar-ruta-predefinida").on('click', function(){
+            var elemento_ruta = $('<div class="rutabox">{!! Form::hidden('predefinido[]', 'yes') !!}<div class="form-group col-sm-4">{!! Form::label('ruta_nombre[]', 'Nombre Ruta') !!}{!! Form::select('ruta_nombre[]', $selectores['ruta_id'], null, ['id'=> 'rutaname', 'class'=> 'form-control select2 rutaname', 'style'=> 'width: 100%', 'placeholder'=>'Seleccione...*', 'required']) !!}</div><div class="form-group col-sm-2">{!! Form::label('distancia[]', 'Distancia') !!}<div class="input-group">{!! Form::text('distancia[]', null, ['class'=> 'form-control texto-right', 'placeholder'=>'0', 'readonly']) !!}<span class="input-group-addon">Km</span> </div></div><div class="form-group col-sm-2">{!! Form::label('duracion[]', 'Duración (Ida-Vuelta)') !!}<div class="input-group">{!! Form::text('duracion[]', null, ['class'=> 'form-control texto-right', 'placeholder'=>'0', 'readonly']) !!}<span class="input-group-addon">minutos</span> </div></div><div class="form-group col-sm-2">{!! Form::label('cantidad[]', 'Cantidad') !!}<div class="input-group">{!! Form::text('cantidad[]', null, ['class'=> 'form-control texto-right', 'placeholder'=>'0' , 'required']) !!}<span class="input-group-addon">Viajes</span> </div></div><div class="form-group col-sm-2">{!! Form::label('valor_final[]', 'Valor C/U') !!}<div class="input-group"> <span class="input-group-addon"><i class="fa fa-usd"></i></span>{!! Form::text('valor_final[]', null, ['class'=> 'form-control texto-right', 'placeholder'=>'0' , 'required']) !!}<span class="input-group-btn eliminar-ruta"><button class="btn btn-danger" type="button"><i class="fa fa-times"></i></button></span></div></div></div><div class="clearfix"></div>');
+
+            elemento_ruta.appendTo('#space-ruta');
+
+            $(".select2").select2({
+              tags: false, // permite insertar texto
+              language: {noResults: function() {return "No se encontraron coincidencias";}, searching: function() {return "Buscando..";}
+                    },
+              placeholder: 'Seleccionar...',      
+              allowClear: true
+            });
+
+            // Iniciador rutas datos otra vez DUPLICADO
+             $(".rutaname").on('change',function(e){ 
+                  var sel_name = $(e.target).val();
+                  buscar_ruta(sel_name);          
+                });
+
+             // Iniciador calculos totales otra vez DUPLICADO
+             $(".texto-right").on('keyup',function(e){
+                calculos_totales();
+              });
+        });
+     
+        // Evento que selecciona la fila y la elimina 
+        $(document).on("click",".eliminar-ruta",function(){
+            var parent = $(this).parents().parents().parents().get(0);
+            //console.log(parent);
+            $(parent).remove();
+        });
+
+        ///////////////////  RUTA NO DEFINIDA
+
+             // Crea el elemento factura
+        $("#agregar-ruta-nueva").on('click', function(){
+            var elemento_ruta = $('<div class="rutabox">{!! Form::hidden('predefinido[]', 'not') !!}<div class="form-group col-sm-4">{!! Form::label('ruta_nombre[]', 'Nombre Ruta') !!}{!! Form::text('ruta_nombre[]', null, ['id'=> 'new_rutaname', 'class'=> 'form-control new_rutaname', 'placeholder'=>'Nueva Ruta', 'required']) !!}</div><div class="form-group col-sm-2">{!! Form::label('distancia[]', 'Distancia') !!}<div class="input-group">{!! Form::text('distancia[]', null, ['class'=> 'form-control texto-right', 'placeholder'=>'0', 'required']) !!}<span class="input-group-addon">Km</span> </div></div><div class="form-group col-sm-2">{!! Form::label('duracion[]', 'Duración (Ida-Vuelta)') !!}<div class="input-group">{!! Form::text('duracion[]', null, ['class'=> 'form-control texto-right', 'placeholder'=>'0', 'required']) !!}<span class="input-group-addon">minutos</span> </div></div><div class="form-group col-sm-2">{!! Form::label('cantidad[]', 'Cantidad') !!}<div class="input-group">{!! Form::text('cantidad[]', 1, ['class'=> 'form-control texto-right', 'placeholder'=>'0', 'required']) !!}<span class="input-group-addon">Viajes</span> </div></div><div class="form-group col-sm-2">{!! Form::label('valor_final[]', 'Valor C/U') !!}<div class="input-group"> <span class="input-group-addon"><i class="fa fa-usd"></i></span>{!! Form::text('valor_final[]', null, ['class'=> 'form-control texto-right', 'placeholder'=>'0', 'required']) !!}<span class="input-group-btn eliminar-ruta"><button class="btn btn-danger" type="button"><i class="fa fa-times"></i></button></span></div></div></div><div class="clearfix"></div>');
+
+            elemento_ruta.appendTo('#space-ruta');
+
+            $(".select2").select2({
+              tags: false, // permite insertar texto
+              language: {noResults: function() {return "No se encontraron coincidencias";}, searching: function() {return "Buscando..";}
+                    },
+              placeholder: 'Seleccionar...',      
+              allowClear: true
+            });
+
+            // Iniciador rutas datos otra vez DUPLICADO
+             $(".rutaname").on('change',function(e){ 
+                  var sel_name = $(e.target).val();
+                  buscar_ruta(sel_name);          
+                });
+
+             // Iniciador calculos totales otra vez DUPLICADO
+             $(".texto-right").on('keyup',function(e){
+                calculos_totales();
+              });
+        });
+     
+        // Evento que selecciona la fila y la elimina 
+        /*
+        $(document).on("click",".eliminar-ruta",function(){
+            var parent = $(this).parents().parents().parents().get(0);
+            //console.log(parent);
+            $(parent).remove();
+        });
+*/
+        /////////////// FIN RURA NO DEFINIDA
+
+
          // Evento que selecciona la fila y la elimina 
         $(document).on("click",".eliminar-descuento",function(){
             var parent = $(this).parents().parents().get(0);
@@ -85,6 +175,45 @@
 
     });
 
+    function calculos_totales(){
+      var subtotal_rutas = 0;
+      var subtotal_descuentos = 0;
+
+     $('input[name^="cantidad"]').each(function( key, value ) {
+           
+           subtotal_rutas += ( $('input[name^="cantidad"]').eq(key).val() * $('input[name^="valor_final"]').eq(key).val() );
+        }); 
+      //console.log("rutas: "+subtotal_rutas);
+      $('#subtotal').val(subtotal_rutas.toFixed(0));
+
+        var desc_admin = parseFloat($('input[name="desc_admin"]').val()) * parseFloat(subtotal_rutas) / 100;
+        var rte_finca  = parseFloat($('input[name="desc_finca"]').val()) * parseFloat(subtotal_rutas) / 100;
+
+        var cuatropormil = parseFloat(0);
+        //console.log("check "+$('#cuatro_por_mil').is(':checked'));        
+        cuatropormil = parseFloat(subtotal_rutas)*4/1000;
+        $('#text_cuatro_por_mil').val(cuatropormil.toFixed(2));     
+        //$('#text_cuatro_por_mil').val(0);
+          var extra_desc = parseFloat(0);
+         $('input[name^="desc_valor"]').each(function( key, value ) {           
+           extra_desc +=  parseFloat($('input[name^="desc_valor"]').eq(key).val());
+          }); 
+
+
+      subtotal_descuentos += parseFloat($('input[name="desc_sobrecosto"]').val()) 
+      +  parseFloat($('input[name="desc_transaccion"]').val()) 
+      +  desc_admin
+      +  rte_finca
+      +  cuatropormil
+      +  extra_desc;
+
+      var total = subtotal_rutas-subtotal_descuentos;
+      //console.log("descuentos: "+subtotal_descuentos+" --- "+subtotal_descuentos.toFixed(2));
+       $('#total_descuentos').val(subtotal_descuentos.toFixed(2));
+
+       $('#total').val(total.toFixed(2));
+    }
+
     function buscar_ruta(ruta_id){
         console.log('Inicio busqueda'); 
         var id = ruta_id;
@@ -97,21 +226,20 @@
          .done(function( data, textStatus, jqXHR ) {
              if ( console && console.log ) {
                  console.log( ">> La solicitud se ha completado correctamente." );
-                console.log(data);
+                // console.log(data);
                 // console.log(data['valor_sugerido']);    
                 
                 //console.log($('input[name^="valor_final"]').eq(1).val());
                 $('input[name^="predefinido"]').each(function( key, value ) {
                     if ($(this).val() == 'yes') {
                       //alert('final: '+ruta);
-                      console.log("ruta: "+ruta_id);
-                      console.log("selector: "+$('select[name^="ruta_nombre"]').eq(key).val());
-                      console.log($('input[select^="ruta_nombre"]').eq(key).val() == ruta_id);
-                      if ($('input[select^="ruta_nombre"]').eq(key).val() == ruta_id) {
-
+                      
+                      if ($('select[name^="ruta_nombre"]').eq(key).val() == ruta_id) {
+                          
+                          $('input[name^="distancia"]').eq(key).val(data['distancia']);
+                          $('input[name^="duracion"]').eq(key).val(data['duracion']);
+                          $('input[name^="cantidad"]').eq(key).val(1);
                           $('input[name^="valor_final"]').eq(key).val(data['valor_sugerido']);
-
-                          console.log(data['valor_sugerido']);
                           
                       }
                       
@@ -119,7 +247,10 @@
                     //alert( key + ": " + value + " valor: "+$(this).val() );
                    // alert($(this).val());
                 });  
-                //fin cambios            
+
+                calculos_totales();
+                //fin cambios 
+
              }            
          })
          .fail(function( jqXHR, textStatus, errorThrown ) {
@@ -128,6 +259,7 @@
              }
         });  
          //return ruta_response;
+
     }
 
  
