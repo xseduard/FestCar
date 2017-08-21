2132728e4cfa7914d215051b8ad80fa211109f6e
Eduard Ramos <xseduard@gmail.com>
2017-08-21 05:36:15 -0500

    change visual transitions and badges normal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -230,25 +230,17 @@
     <script type="text/javascript">
         $(document).ready(function() {
           $(".animsition").animsition({
-            inClass: 'fade-in',
-            outClass: 'fade-out',
-            inDuration: 1500,
-            outDuration: 800,
-            linkElement: '.animsition-link',
-            // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+            inClass: 'fade-in-left-sm',
+            outClass: 'fade-out-left-sm',
+            inDuration: 500,
+            outDuration: 350,
+            linkElement: 'a:not([target="_blank"]):not([href^="#"])',
             loading: true,
-            loadingParentElement: 'body', //animsition wrapper element
+            loadingParentElement: '.content', //animsition wrapper element
             loadingClass: 'animsition-loading',
             loadingInner: '', // e.g '<img src="loading.svg" />'
-            timeout: false,
-            timeoutCountdown: 5000,
             onLoadEvent: true,
             browser: [ 'animation-duration', '-webkit-animation-duration'],
-            // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-            // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-            overlay : false,
-            overlayClass : 'animsition-overlay-slide',
-            overlayParentElement : 'body',
             transition: function(url){ window.location.href = url; }
           });
           console.log('%cReady To FestCar - PowerBy XSEDUARD', 'color:green;');          
@@ -257,6 +249,8 @@
      <!-- Animsition end ini -->
     @yield('scripts')
     @stack('scripts')
-    @include('common.partial_chat')
+    @if ( config('app.env') == 'production' )
+        @include('common.partial_chat')    
+    @endif
 </body>
 </html>
